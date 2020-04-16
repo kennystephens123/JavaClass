f97483b5b1cb11187379bfb8340a86d4f1094d1e
kennystephens123 <kenstephen825@gmail.com>
2020-04-15 21:25:41 -0400

    first commit

---
+++ src/com/techcircle/java/basic/HelloWorld.java
@@ -0,0 +1,33 @@
+package com.techcircle.java.basic;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello TechCircle!!");
+		String make = "Mercedes";
+		System.out.println(make);
+
+		String model = "E 300";
+		System.out.println(model);
+
+		String year = "2019";
+		System.out.println(year);
+	
+	String name;
+	
+	String $;
+	
+	String _;
+	
+	String s8923477218970;
+	
+	int num777888;
+	
+	int _$$$$$$$$$;
+	
+	
+	
+	
+	
+	}
+}
+
